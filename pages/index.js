afa68c303d76ba7d91c01f7147d103207f12ad26
luisholman <88738669+luisholman@users.noreply.github.com>
2023-08-14 17:19:56 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { PrismaClient } from '@prisma/client'
+import { categorias } from '@/prisma/data/categorias'
+
 
 export default function Home() {
+  console.log(categorias);
   return (
-    <h1>hola</h1>
+    
+    <h1>next.js</h1>
   )
+}
+
+export const getServerSideProps= async ()=>{
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany()
+  
+  return{
+    props:{
+      categorias,
+    }
+  }
 }
